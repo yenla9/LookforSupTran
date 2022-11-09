3d59f2484c0f0812efce9eb88e46b519410b4653
yenla9@gmail.com <yenla9@gmail.com>
2022-11-08 21:42:32 -0800

    update

--- Part One.sql
+++ Part One.sql
@@ -1,12 +1,8 @@
 -- Isolate (or group) the transactions of each cardholder
--- Count the transactions that are less than $2.00 per cardholder.
---View the credit_holder table
-SELECT*
-FROM credit_holder;
-
---View the credit_trans table
-SELECT*
-FROM credit_trans;
+SELECT cardholder_id, count(*) AS transaction_count
+FROM credit_trans
+GROUP by cardholder_id
+ORDER BY (count(*))DESC;
 
 -- Show count the transactions that are less than $2.00 per cardholder.
 SELECT cardholder_id, count(*) AS transaction_count
@@ -15,45 +11,6 @@ WHERE amount < Money(2.00)
 GROUP by cardholder_id
 ORDER BY (count(*))DESC;
 
---Validate name and transaction with merchant purchase
-SELECT *
-FROM credit_trans as a
-INNER JOIN merchant as b ON a.id_merchant = b.merchant_id
-WHERE CAST (a.name AS text) = 'Stephanie Dalton' AND amount < Money(2.00)
-
--- Show count the transactions that are less than $2.00 per card.
-SELECT card, count(*) AS transaction_count
-FROM transaction
-WHERE(transaction.card IN 
-	   ( SELECT credit_card.card
-         FROM credit_card
-         WHERE (credit_card.cardholder_id IN ( 
-			 SELECT card_holder.id
-             FROM card_holder
-		 	 ))))
- AND amount < Money(2.00)
- GROUP BY card
- ORDER BY (count(*)) DESC;
-
- -- Top 3 validations for card after joining with merchant
- SELECT *
- FROM transaction as a
- INNER JOIN merchant as b ON a.id_merchant = b.merchant_id
- WHERE CAST (card AS bigInt)= 376027549341849 AND amount < Money(2.00)
- ORDER BY id_merchant;
-
- SELECT *
- FROM transaction as a
- INNER JOIN merchant as b ON a.id_merchant = b.merchant_id
- WHERE CAST (card AS bigInt)= 584226564303 AND amount < Money(2.00)
- ORDER BY id_merchant;
- 
- SELECT *
- FROM transaction as a
- INNER JOIN merchant as b ON a.id_merchant = b.merchant_id
- WHERE CAST (card AS bigInt)= 344119623920892 AND amount < Money(2.00)
- ORDER BY id_merchant;
- 
 -- Is there any evidence to suggest that a credit card has been hacked? Explain your rationale.
 -- No I didn't see any evidence to suggest that a credit card has been hacked. It didnt have repeated
 -- charges from a merchant that would raise a red flag. 
@@ -70,21 +27,21 @@ LIMIT 100;
 --No I don't see any transaction that could be fraudulent. 
 
 --Is there a higher number of fraudulent transactions made during this time frame versus the rest of the day?
-SELECT amount, COUNT(*) AS transaction_count
+SELECT amount,  COUNT(*) AS transaction_count
 FROM credit_trans
 WHERE CAST(date as TIME) >='21:00:00' and CAST(date as TIME) <= '24:00:00'
 GROUP BY amount
 ORDER BY (amount) DESC;
 
 --There are much more transactions from 21:00 to 24:00 compared to other time interval. 
---In addition, 1:00 to 3:00 and 3:00 to 5:00 most people are asleep and there are still transactions.
---I would look more closely to these timeframes.
+--In addition, 1:00am to 3:00am and 3:00am to 5:00am most people are asleep and there are still transactions.
+--I would look into these timeframes.
 
 --What are the top 5 merchants prone to being hacked using small transactions?
-SELECT merchant_name, Count(*) as transaction_count
+SELECT name, Count(*) as transaction_count
 FROM merchant_trans
 WHERE amount <Money(2.00)
-GROUP BY merchant_name
+GROUP BY name
 ORDER BY (count(*)) DESC
 LIMIT 5;
 
--- schema.sql
+++ schema.sql
@@ -1,43 +1,86 @@
-CREATE TABLE card_holder(
- id integer NOT NULL,
- name VARCHAR(255) NOT NULL);
- 
- CREATE TABLE credit_card(
- card VARCHAR(20) NOT NULL,
- cardholder_id integer NOT NULL);
- 
- CREATE TABLE merchant_catgory(
- id integer NOT NULL,
- name VARCHAR(255) NOT NULL);
- 
- CREATE TABLE merchant(
- id integer NOT NULL,
- name VARCHAR(255) NOT NULL,
- id_merchant_category integer NOT NULL);
- 
- CREATE TABLE transaction (
- id integer NOT NULL,
- date timestamp without time zone NOT NULL,
- amount money,
- card VARCHAR(20) NOT NULL,
- id_merchant integer NOT NULL);
+DROP TABLE IF EXISTS cardholder CASCADE;
+DROP TABLE IF EXISTS credit_card CASCADE;
+DROP TABLE IF EXISTS merchant_catgory CASCADE;
+DROP TABLE IF EXISTS transaction CASCADE;
+DROP TABLE IF EXISTS credit_holder CASCADE;
+DROP TABLE IF EXISTS credit_trans CASCADE;
+DROP TABLE IF EXISTS merchant_trans CASCADE;
+DROP TABLE IF EXISTS merchant CASCADE;
+DROP TABLE IF EXISTS card_holder CASCADE;
 
---create a new table after joing credit card and credit holder
+--Create card_holder Table------------------------
+CREATE TABLE card_holder(
+id integer NOT NULL PRIMARY KEY,
+name VARCHAR(255) NOT NULL);
+
+--Create credit_card Table------------------------
+CREATE TABLE credit_card(
+card VARCHAR(20) NOT NULL PRIMARY KEY,
+cardholder_id integer NOT NULL,
+FOREIGN KEY (cardholder_id) REFERENCES card_holder(id));
+
+--Create merchant_catgory Table------------------------
+CREATE TABLE merchant_catgory(
+id integer NOT NULL PRIMARY KEY,
+name VARCHAR(255) NOT NULL);
+
+--Create merchant Table------------------------
+CREATE TABLE merchant(
+id integer NOT NULL PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+id_merchant_category integer NOT NULL,
+FOREIGN KEY (id_merchant_category) REFERENCES merchant_catgory(id));
+
+--Create transaction Table------------------------
+CREATE TABLE transaction (
+id integer NOT NULL PRIMARY KEY,
+date timestamp without time zone NOT NULL,
+amount money,
+card VARCHAR(20) NOT NULL,
+FOREIGN KEY (card) REFERENCES credit_card(card),
+id_merchant integer NOT NULL,
+FOREIGN KEY (id_merchant) REFERENCES merchant(id));
+
+--create a new table, credit_holder, after joing credit card and credit holder
 CREATE TABLE credit_holder AS
 SELECT * FROM credit_card as a JOIN card_holder as b ON(a.cardholder_id = b.id);
 
---Remove id column 
+--Rename COLUMN to credit_holder_card for joining with other tables
 ALTER TABLE credit_holder
+RENAME COLUMN card TO credit_holder_card;
+
+--Add Primary Key and drop duplicate column
+ALTER TABLE credit_holder
+ADD PRIMARY KEY (credit_holder_card),
+ADD FOREIGN KEY (cardholder_id) REFERENCES card_holder(id),
+ADD FOREIGN KEY (credit_holder_card) REFERENCES credit_card(card),
 DROP COLUMN id;
 
 --create a new table after joining credit holder and transaction
 CREATE TABLE credit_trans AS
-SELECT * FROM transaction as a JOIN credit_holder as b ON(a.card = b.card_holder);
+SELECT * FROM transaction as a JOIN credit_holder as b ON(a.card = b.credit_holder_card);
 
--- Remove card_holder column
+--Rename COLUMN to tran_id for joining with other tables
 ALTER TABLE credit_trans
-DROP COLUMN card_holder;
+RENAME COLUMN id TO tran_id;
+
+--Rename COLUMN to credit_name for joining with other tables
+ALTER TABLE credit_trans
+RENAME COLUMN name TO credit_name;
+
+-- Add PRIMARY KEY for credit_trans table and Remove card_holder_card column
+ALTER TABLE credit_trans
+ADD PRIMARY KEY (tran_id),
+ADD FOREIGN KEY (id_merchant) REFERENCES merchant(id),
+DROP COLUMN credit_holder_card;
 
 --create a new table after joining credit_trans and merchant
 CREATE TABLE merchant_trans AS
-SELECT * FROM credit_trans as a JOIN merchant as b ON(a.id_merchant = b.merchant_id);
+SELECT * FROM credit_trans as a JOIN merchant as b ON(a.id_merchant = b.id);
+
+-- Remove merchant_id column
+ALTER TABLE merchant_trans
+ADD PRIMARY KEY (tran_id),
+ADD FOREIGN KEY (id_merchant) REFERENCES merchant(id),
+DROP COLUMN id;
+
